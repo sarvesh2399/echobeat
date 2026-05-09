2b8adc240991653aecf50d65a81661986b850fc7
Sarvesh Patil <sarveshspatil99@gmail.com>
2026-05-09 18:23:35 +0530

    Create auth middleware and use multer

--- server/src/controllers/music.controller.js
+++ server/src/controllers/music.controller.js
@@ -1,33 +1,16 @@
 const musicModel = require("../models/music.model");
 const { uploadFile } = require("../services/storage.service");
 const jwt = require("jsonwebtoken");
+const albumModel = require("../models/album.model");
 
 async function createMusic(req, res) {
-    const token = req.cookies.token;
-    if (!token) {
-        return res.status(401).json({
-            message: "Unauthorized",
-        });
-    }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if (decoded.role !== "artist") {
-            return res.status(401).json({
-                message: "You dont have access to create a music track",
-            });
-        }
-    } catch (err) {
-        return res.status(401).json({
-            message: "Unauthorized",
-        });
-    }
     const { title } = req.body;
     const file = req.file;
     const result = await uploadFile(file.buffer.toString("base64"));
     const music = await musicModel.create({
         uri: result.url,
         title,
-        artist: decoded.id,
+        artist: req.user.id,
     });
     res.status(201).json({
         message: "Music track created successfully",
@@ -40,6 +23,26 @@ async function createMusic(req, res) {
     });
 }
 
+async function createAlbum(req, res) {
+    const { title, musics } = req.body;
+
+    const album = await albumModel.create({
+        title,
+        artist: req.user.id,
+        musics: musics,
+    });
+
+    res.status(201).json({
+        message: "Album created successfully",
+        album: {
+            id: album._id,
+            title: album.title,
+            artist: album.artist,
+            musics: album.musics,
+        },
+    });
+}
 module.exports = {
-    createMusic
+    createMusic,
+    createAlbum,
 };
--- server/src/routes/music.routes.js
+++ server/src/routes/music.routes.js
@@ -1,9 +1,14 @@
 const express = require("express")
-
+const multer = require("multer")
 const router = express.Router()
-
 const musicController = require("../controllers/music.controller")
+const authMiddleware = require("../midlewares/auth.middleware")
 
-router.post("/upload", musicController.createMusic)
+const upload = multer({
+    storage: multer.memoryStorage()
+})
+
+router.post("/upload", authMiddleware.authArtist, upload.single("music"), musicController.createMusic)
+router.post("/album", authMiddleware.authArtist, musicController.createAlbum)
 
 module.exports = router
